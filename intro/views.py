b308a4697b0b55354d0b0b839f47ac8fc9077a75
Vinícius Chan <viniciuschan@hotmail.com>
2018-01-08 23:16:34 -0200

    tutorial 04 - generic views

--- intro/views.py
+++ intro/views.py
@@ -1,32 +1,28 @@
-from django.shortcuts import render, get_object_or_404, render
-from django.template import loader
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question, Choice
+from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
-    template = loader.get_template('intro/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))    
 
-def detail(request, question_id):
-    """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'intro/detail.html', {'question': question})
-    """
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'intro/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'intro/index.html'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'intro/results.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'intro/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'intro/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
